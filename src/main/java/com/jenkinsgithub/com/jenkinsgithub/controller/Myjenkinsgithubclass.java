1ce4b08338ff3a32b1c65637d55b40442d9c647e
RakeshPrimers <tadepu143@gmail.com>
2020-01-20 22:46:09 +0530

    hello second

--- src/main/java/com/jenkinsgithub/com/jenkinsgithub/controller/Myjenkinsgithubclass.java
+++ src/main/java/com/jenkinsgithub/com/jenkinsgithub/controller/Myjenkinsgithubclass.java
@@ -1,10 +1,13 @@
 package com.jenkinsgithub.com.jenkinsgithub.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Myjenkinsgithubclass {
 	
+	
+  @RequestMapping("/hellojenkins")
 	public String hellojenkins()
 	{
 		return "Hello Jenkins";
