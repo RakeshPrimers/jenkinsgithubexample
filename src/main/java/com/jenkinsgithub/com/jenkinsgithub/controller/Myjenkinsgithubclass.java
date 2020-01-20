6f6c75f7f8d72a6f1b5fd47709a8ddf320f19d9e
RakeshPrimers <tadepu143@gmail.com>
2020-01-20 21:16:36 +0530

    using github jenkins now

---
+++ src/main/java/com/jenkinsgithub/com/jenkinsgithub/controller/Myjenkinsgithubclass.java
@@ -0,0 +1,13 @@
+package com.jenkinsgithub.com.jenkinsgithub.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Myjenkinsgithubclass {
+	
+	public String hellojenkins()
+	{
+		return "Hello Jenkins";
+	}
+
+}
